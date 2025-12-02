9521e95e3cfdd6bd45599c1b74e1a201f26af30e
h3nc4 <me@h3nc4.com>
2025-12-01 21:50:23 -0300

    update publishing url to wasudoku.h3nc4.com

--- vite.config.ts
+++ vite.config.ts
@@ -26,7 +26,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/WASudoku/',
+  base: '/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
